50b8f4cf77acf9ee9865fff8807af4b31450628a
Kristyan Yochev <kris.yochev@gmail.com>
2020-01-18 13:52:45 +0200

    [Update] Added offer model

---
+++ server/online_store/models/offer.py
@@ -0,0 +1,33 @@
+from online_store.extensions import db
+
+
+class OfferModel(db.Model):
+    __tablename__ = "offer"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    publication_date = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp()
+    )
+    active = db.Column(db.Boolean, default=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    buyer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "publication_date": str(self.publication_date),
+            "active": self.active,
+            "author": self.author.to_json(),
+            "buyer": self.buyer.to_json() if self.buyer else None
+        }
